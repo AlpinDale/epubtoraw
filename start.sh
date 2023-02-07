a85e18ac0f68ca869f26b547f4db5fba96e73850
AlpinDale <52078762+AlpinDale@users.noreply.github.com>
2023-02-07 23:39:06 +0430

    Added batch conversion support

--- start.sh
+++ start.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-# Get the file name from user with this:
-read -e -p "Enter the name of the epub file: " file
+# Get all the .epub files in the current directory
+epub_files=($(ls *.epub))
 
-# Run the go script and print out a list of the chapter numberings
-chapters=$(./etg "$file")
+# Loop through all the .epub files
+for file in "${epub_files[@]}"; do
+	# Escape any spaces in the file name
+	file_escaped=${file// /\\ }
 
-# Extract chapter number and use it to re-run the go script and print out our raw text
-for chapter in $chapters; do
-	number=$(echo $chapter | awk -F/ '{print $1}')
-	./etg "$file" $number >> "$file".txt
+	# Run the go script and print out a list of the chapter numberings
+	chapters=$(./etg "$file_escaped")
+
+	# Use pv to display a progress bar while extracting each chapter
+	for chapter in $chapters; do
+		number=$(echo $chapter | awk -F/ '{print $1}')
+		./etg "$file_escaped" $number | pv -t -e -b -a -N "Extracting chapter $number" >> "${file%.epub}.txt"
+	done
 done
